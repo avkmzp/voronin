d70603102e4b1f1c44e9a7a4dff450d0c9481b76
avkmzp <avkmzp@gmail.com>
2018-01-25 00:51:06 +0300

    Редирект на HTTPS, таблицы с тренировками в новой вкладке

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+const NAME_OF_SERVER = 'growinghome.ru';
+if ($_SERVER['HTTP_HOST'] == NAME_OF_SERVER and !$_SERVER['HTTPS']) {
+    header('Location: https://' . NAME_OF_SERVER . '/');
+}
+?>
+<!DOCTYPE html>
+<html lang='ru-RU'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='theme-color' content='#98ad13'>
+    <title>Тренировочная зона</title>
+    <link rel='stylesheet' href='//growinghome.ru/styles/style.css?=002'>
+    <link rel='icon' href='favicon.ico'>
+</head>
+<body>
+<div>
+    <div class='tri'>
+        <a href='timer.html'>Таймер</a>
+    </div>
+    <div class='tri'>
+        <a href='users.html' target='_blank'>План</a>
+    </div>
+    <!--<div class='tri'>-->
+    <!--<a href='fitnestimer.html'>Таймер</a>-->
+    <!--</div>-->
+</div>
+</body>
+</html>
